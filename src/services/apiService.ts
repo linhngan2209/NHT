1efe81f0f2e3df6693ee953eb4a2613bd960f6d9
linhngan2209 <nganptlhe160415@fpt.edu.vn>
2024-08-25 20:14:27 +0700

    fix upi

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:8080/', 
+    baseURL: 'https://ev-charging-server-z3gr3glbrq-de.a.run.app/', 
     timeout: 10000,                      
     headers: {
         'Content-Type': 'application/json',
     },
 });
 
-// Add a request interceptor
 axiosClient.interceptors.request.use(
     function (config) {
         const token = localStorage.getItem('token');
